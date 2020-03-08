01b764480d5de8ea65543e93c1143c00f7d786bb
ivan_ <ivan_@LAPTOP-LVTM11G4>
2020-03-08 18:37:31 +0100

    Trabakdno en la clase ASumar, creando método

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -1,5 +1,32 @@
 package sumador;
 
-public class ASumar {
+import java.util.Scanner;
 
+public class ASumar {
+	
+	int i;
+	public ASumar(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void laSuma(int i) {
+		 while(i > 0) {
+
+	            int resultado = i % 10;
+
+	            i = i / 10;
+
+	        }
+
+	        System.out.println("La suma es: " + i);
+		
+	}
+	
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un número para averiguar la suma de sus dígitos: ");
+		int num=sc.nextInt();
+		laSuma(num);
+	}
 }
